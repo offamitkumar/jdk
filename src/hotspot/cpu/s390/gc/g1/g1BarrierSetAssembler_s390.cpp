5d5cc475adfe27c36c9a3bc4bf4b17e1891f1050
Amit Kumar <amitlocham09@icloud.com>
2024-08-02 12:35:19 +0530

    cleanuP

--- src/hotspot/cpu/s390/gc/g1/g1BarrierSetAssembler_s390.cpp
+++ src/hotspot/cpu/s390/gc/g1/g1BarrierSetAssembler_s390.cpp
@@ -51,19 +51,6 @@
 
 #define BLOCK_COMMENT(str) __ block_comment(str)
 
-static Assembler::branch_condition generate_marking_active_test(MacroAssembler* masm, const Register thread, Register tmp1) {
-  const int active_offset = in_bytes(G1ThreadLocalData::satb_mark_queue_active_offset());
-  // Is marking active?
-  // Note: value is loaded for test purposes only. No further use here.
-  if (in_bytes(SATBMarkQueue::byte_width_of_active()) == 4) {
-    __ load_and_test_int(tmp1, Address(Z_thread, active_offset));
-  } else {
-    guarantee(in_bytes(SATBMarkQueue::byte_width_of_active()) == 1, "Assumption");
-    __ load_and_test_byte(tmp1, Address(Z_thread, active_offset));
-  }
-  return Assembler::bcondNotEqual;
-}
-
 void G1BarrierSetAssembler::gen_write_ref_array_pre_barrier(MacroAssembler* masm, DecoratorSet decorators,
                                                             Register addr, Register count) {
   bool dest_uninitialized = (decorators & IS_DEST_UNINITIALIZED) != 0;
@@ -144,11 +131,6 @@ void G1BarrierSetAssembler::g1_write_barrier_pre_c2(MacroAssembler* masm,
 
   stub->initialize_registers(obj, pre_val, thread, tmp1, tmp2);
   
-  if (UseNewCode) {
-  Assembler::branch_condition bcond = generate_marking_active_test(masm, thread, tmp1);
-  __ branch_optimized(bcond, *stub->entry()); // Activity indicator is zero, so there is no marking going on currently.
-  } else {
-  // FIXME: Below code could be moved to a function, for now it's fine :-)
   const int active_offset = in_bytes(G1ThreadLocalData::satb_mark_queue_active_offset());
   // Is marking active?
   // Note: value is loaded for test purposes only. No further use here.
@@ -159,7 +141,7 @@ void G1BarrierSetAssembler::g1_write_barrier_pre_c2(MacroAssembler* masm,
     __ load_and_test_byte(tmp1, Address(Z_thread, active_offset));
   }
   __ branch_optimized(Assembler::bcondNotEqual, *stub->entry()); // Activity indicator is zero, so there is no marking going on currently.
-  }
+
   __ bind(*stub->continuation());
   
   BLOCK_COMMENT("} g1_write_barrier_pre_c2");
