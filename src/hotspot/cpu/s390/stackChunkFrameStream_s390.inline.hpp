7d756c11dd4b6c691d32e43754683ec720dd24b2
Amit Kumar <amitlocham09@icloud.com>
2025-02-18 06:03:59 +0000

    some fix

--- src/hotspot/cpu/s390/stackChunkFrameStream_s390.inline.hpp
+++ src/hotspot/cpu/s390/stackChunkFrameStream_s390.inline.hpp
@@ -99,8 +99,16 @@ inline int StackChunkFrameStream<frame_kind>::interpreter_frame_stack_argsize() 
 
 template <ChunkFrames frame_kind>
 inline int StackChunkFrameStream<frame_kind>::interpreter_frame_num_oops() const {
-  Unimplemented();
-  return 0;
+  assert_is_interpreted_and_frame_type_mixed();
+  ResourceMark rm;
+  InterpreterOopMap mask;
+  frame f = to_frame();
+  f.interpreted_frame_oop_map(&mask);
+  return  mask.num_oops()
+          + 1 // for the mirror oop
+          + (f.interpreter_frame_method()->is_native() ? 1 : 0) // temp oop slot
+          + pointer_delta_as_int((intptr_t*)f.interpreter_frame_monitor_begin(),
+                                 (intptr_t*)f.interpreter_frame_monitor_end())/BasicObjectLock::size();
 }
 
 template<>
