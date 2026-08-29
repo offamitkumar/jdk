051c87be057310ae34e2487a2cd51a1498566328
Amit Kumar <amitlocham09@icloud.com>
2026-08-29 10:07:52 +0000

    enable

--- src/hotspot/cpu/s390/globals_s390.hpp
+++ src/hotspot/cpu/s390/globals_s390.hpp
@@ -78,8 +78,8 @@ define_pd_global(bool, CompactStrings, true);
 // 8146801 (Short Array Allocation): No performance work done here yet.
 define_pd_global(intx, InitArrayShortSize, 1*BytesPerLong);
 
-define_pd_global(bool, InlineTypePassFieldsAsArgs, false);
-define_pd_global(bool, InlineTypeReturnedAsFields, false);
+define_pd_global(bool, InlineTypePassFieldsAsArgs, true);
+define_pd_global(bool, InlineTypeReturnedAsFields, true);
 
 #define ARCH_FLAGS(develop,                                                   \
                    product,                                                   \
